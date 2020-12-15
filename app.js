6e5ad7f33ea9d4078e94114a4fc0b597803fbab2
Poonammohata <poonammohata2@mail.com>
2020-12-15 17:31:59 +0530

    div show output added div and showntext on it on click

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var textInput=document.querySelector("#text-input");
+var textTranslated=document.querySelector("#text-translated");
 
 
 btnTranslate.addEventListener("click",function clickEventHandler(){
     console.log(textInput.value);
+    textTranslated.innerText="hello i am poonam";
 })
