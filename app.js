6299e49a8fe4a2a3be0401c4d4fa8cd742d6138c
Poonammohata <poonammohata2@mail.com>
2020-12-15 17:53:01 +0530

    fetch call error handling

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ var btnTranslate=document.querySelector("#btn-translate");
 var textInput=document.querySelector("#text-input");
 var textTranslated=document.querySelector("#text-translated");
 
+var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+function urlUpdate(text){
+    var urlExtension=url+"?text="+text;
+    console.log(urlExtension)
+    return urlExtension;
+}
+function errorHandler(error){
+    console.log("error occured "+error);
+    alert("there is some problem at server,checkafter some time");
+}
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log(textInput.value);
-    textTranslated.innerText="hello i am poonam";
+    var textInputValue=textInput.value;
+    fetch(urlUpdate(textInputValue))
+    .then(function responseConverter(response){
+        return response.json();
+    })
+    .then(function jsonOutput(json){
+        var texttrans=json.contents.translated
+        textTranslated.innerHTML=texttrans;
+    })
+    .catch(errorHandler)
 })
